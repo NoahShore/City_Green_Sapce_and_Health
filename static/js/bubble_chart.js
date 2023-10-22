20a430c23af3ca9a48ddcf3812f436f3ce3e7ccb
GabrielaDelgado <delga132@umn.edu>
2023-10-22 18:13:50 -0500

    New library chart

--- static/js/bubble_chart.js
+++ static/js/bubble_chart.js
@@ -4,17 +4,26 @@ const mongo_endpoint='mongo';
 
 
 d3.json(mongo_endpoint).then(function(data) {
-    console.log(data)
-
+    console.log(data.length)
+-
 console.log(data.length)
 
 let list = []      
 
-for (let i = 0; i < data.length; i++) {
+for (let i = 0; i < data; i++) {
     list.push(data[i].ob_data_value)
 };
 
 console.log(list)
+
+var mongo_endpoint = 'mongodb://localhost:27017/green_spaces_and_health';
+
+$.getJSON(mongo_endpoint, function(response) {
+  chart.updateSeries([{
+    name: 'bubble_chart',
+    data: response
+  }])
+});
 
 var options = {
     series: [{
@@ -38,7 +47,26 @@ var options = {
       max: 70
   }
   };
-  let Bubblechart = new ApexCharts(document.querySelector("#bubble_plot"), options);
+  let Bubblechart = new ApexCharts(document.querySelector("#bubble_chart"), options);
   Bubblechart.render();
 
 });
+
+// const mongo_endpoint='mongo';
+
+// d3.json(mongo_endpoint).then(function(data) {
+//   console.log(data);
+
+// const config = {
+//   type: 'scatter',
+//   data: data,
+//   options: {
+//     scales: {
+//       x: {
+//         type: 'linear',
+//         position: 'bottom'
+//       }
+//     }
+//   }
+// };
+// });
