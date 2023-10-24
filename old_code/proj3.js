7ce505ff01a7a4281ffe5fbde61e4d62070fe128
NoahShore <shore048@umn.edu>
2023-10-23 21:36:23 -0500

    cleaning up folder directory

--- old_code/proj3.js
+++ old_code/proj3.js
@@ -1,39 +1,46 @@
 // Place url in a constant variable
-//const url = "mongodb://localhost:8000/green_space_and_health"
 const mongo_endpoint='mongo';
 // Fetch the JSON data and console log it
-d3.json(city_data, function(data) {
+d3.json(mongo_endpoint).then(function(data) {
   console.log(data);
-});
+
+
+
+let cities = []
+
+ for (let i = 0; i < data.length; i++) {
+     cities.push(data[i].city);
+   }
+console.log(cities);
 
 // Initialize the dashboard at start up 
 function init() {
-
     // Use D3 to select the dropdown menu
     let dropdownMenu = d3.select("#selDataset");
 
     // Use D3 to get sample names and populate the drop-down selector
-    d3.json(city_data).then((data) => {
+    d3.json(mongo_endpoint).then((data) => {
+        console.log(data)
         
         // Set a variable for the sample names
-        let city = data.city;
 
+        let city_name = data.city;
+    
         // Add  samples to dropdown menu
-        city.forEach((id) => {
-
+        data.city.forEach(function(city_name) {
             dropdownMenu.append("option")
-            .text(city)
-            .property("value",city);
+            .text(city_name)
+            .property("value",city_name);
         });
 
         // Set the first sample from the list
-        let sample_one = city[0];
+        let sample_one = cities[0];
 
         // Log the value of sample_one
         console.log(sample_one);
 
-        // Build the initial plots
-        buildInfoData(sample_one);
+        // // Build the initial plots
+        // buildMetadata(sample_one);
         // buildBarChart(sample_one);
         // buildBubbleChart(sample_one);
         // buildGaugeChart(sample_one);
@@ -42,16 +49,17 @@ function init() {
 };
 
 // Function that populates metadata info
-function buildInfoData(sample) {
+function demoInfo(city_name) {
 
     // Use D3 to retrieve all of the data
-    d3.json(city_data).then((data) => {
+    d3.json(mongo_endpoint).then((data) => {
 
-        // Retrieve all data
-        let info = data.city_data;
+        // Retrieve all metadata
+        let info = data;
+        console.log(data)
 
         // Filter based on the value of the sample
-        let value = info.filter(result => result.id == sample);
+        let value = buildInfo.filter(result => result.id == sample);
 
         // Log the array of metadata objects after the have been filtered
         console.log(value)
@@ -60,7 +68,7 @@ function buildInfoData(sample) {
         let valueData = value[0];
 
         // Clear out metadata
-        d3.select("#sample-info").html("");
+        d3.select("#sample-metadata").html("");
 
         // Use Object.entries to add each key/value pair to the panel
         Object.entries(valueData).forEach(([key,value]) => {
@@ -68,7 +76,7 @@ function buildInfoData(sample) {
             // Log the individual key/value pairs as they are being appended to the metadata panel
             console.log(key,value);
 
-            d3.select("#sample-info").append("h5").text(`${key}: ${value}`);
+            d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
         });
     });
 
@@ -90,17 +98,17 @@ function buildBarChart(sample) {
         let valueData = value[0];
 
         // Get the otu_ids, lables, and sample values
-        let ob_data = valueData.ob_data_value;
-        let mh_data = valueData.mh_data_value;
-        let dv_data = valueData.dv_data_value;
+        let otu_ids = valueData.otu_ids;
+        let otu_labels = valueData.otu_labels;
+        let sample_values = valueData.sample_values;
 
         // Log the data to the console
-        console.log(ob_data,mh_data,dv_data);
+        console.log(otu_ids,otu_labels,sample_values);
 
         // Set top ten items to display in descending order
-        let yticks = ob_data.slice(0,10).map(id => `OTU ${id}`).reverse();
-        let xticks = dv_data.slice(0,10).reverse();
-        let labels = mh_data.slice(0,10).reverse();
+        let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
+        let xticks = sample_values.slice(0,10).reverse();
+        let labels = otu_labels.slice(0,10).reverse();
         
         // Set up the trace for the bar chart
         let trace = {
@@ -113,10 +121,11 @@ function buildBarChart(sample) {
 
         // Setup the layout
         let layout = {
-            title: "Top 10 Health"
+            title: "Top 10 OTUs Present"
         };
 
         // Call Plotly to plot the bar chart
         Plotly.newPlot("bar", [trace], layout)
     });
 };
+});
